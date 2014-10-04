b70c1fdf5a50eb14bc1a8fab00d74d0d67fa70d0
Lindsay Simpkins <LWSimpkins@users.noreply.github.com>
2014-10-04 14:27:06 -0400

    Added class for the thread; implements Runnable

---
+++ src/parallelfibonacci/Fibonacci.java
@@ -0,0 +1,26 @@
+/*
+Fibonacci.java
+Lindsay Simpkins
+COMP 755
+10/4/14
+
+Is given n and an array.
+Calculates the Fibonacci sequence from 0-n.
+Stores the sequence in the array.
+ */
+
+package parallelfibonacci;
+
+public class Fibonacci implements Runnable{
+    private int n;
+    private int[] a;
+    
+    public Fibonacci(int n, int[] a){
+        this.n = n;
+        this.a = a;
+    }
+    
+    public void run(){
+        
+    }
+}
